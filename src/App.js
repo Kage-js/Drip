2010136e3ec8b84fc6df21add832fed256a4b4b9
ericfrancis313 <48592818+ericfrancis313@users.noreply.github.com>
2022-08-12 18:09:54 -0400

    added dashboard and API connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+ 
+import { useEffect, useState } from 'react';
 import './App.css';
+import Dashboard from './components/layouts/Dashboard';
 
 function App() {
+
+  const [weather, setWeather] = useState([]);
+  const [city, setCity] = useState('');
+
+  const getWeather = async () => {
+    const response = await fetch(`http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/02136?apikey=${API_KEY}`);
+    const data = await response.json();
+    setWeather(currentList => [...currentList, data]);
+    setCity('Boston');
+    
+    weather[0].map((forcast, index) => (    
+      console.log(forcast)
+      
+    ))
+  }
+  useEffect(() => {
+    getWeather()
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dashboard weather={weather[0]} city={city} />
+      {city}
     </div>
   );
 }
---
+++ src/components/layouts/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component, Button } from 'react'
+
+
+export default class Dashboard extends Component {
+    render() {
+        
+        return (
+        <div>
+            <h1>Dashboard</h1>
+
+        </div>
+    )
+  }
+}
